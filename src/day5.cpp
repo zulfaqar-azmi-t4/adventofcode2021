347de27bf6dde0b95b21f74d77a6386994d6043e
Zulfaqar Azmi <mzulfaqar88@gmail.com>
2021-12-14 23:53:59 +0900

    DAy 5 Part 1

--- src/day5.cpp
+++ src/day5.cpp
@@ -27,31 +27,39 @@ int main(int argc, char *argv[])
 
   std::vector<std::pair<Point, Point>> lines {};
 
-  int max_x = 0;
-  int max_y = 0;
-  while(input >> first_column >> second_column >> third_column)
-    {
-      Point s;
-      std::string number {};
-      std::stringstream ss(std::move(first_column));
-      std::getline(ss, number, ',');
-      s.x = stoi(number);
-      std::getline(ss, number);
-      s.y = stoi(number);
-      max_x = std::max(max_x, s.x);
-      max_y = std::max(max_y, s.y);
+  int min_x = std::numeric_limits<int>::max();
+  int min_y = std::numeric_limits<int>::max();
+  int max_x = std::numeric_limits<int>::min();
+  int max_y = std::numeric_limits<int>::min();
+  while (input >> first_column >> second_column >> third_column) {
+    Point s;
+    std::string number{};
+    std::stringstream ss(std::move(first_column));
+    std::getline(ss, number, ',');
+    s.x = stoi(number);
+    std::getline(ss, number);
+    s.y = stoi(number);
+    
+    min_x = std::min(min_x, s.x);
+    min_y = std::min(min_y, s.y);
 
-      Point e;
-      std::stringstream ee(std::move(third_column));
-      std::getline(ee, number, ',');
-      e.x = stoi(number);
-      std::getline(ee, number);
-      e.y = stoi(number);
-      max_x = std::max(max_x, e.x);
-      max_y = std::max(max_y, e.y);
+    max_x = std::max(max_x, s.x);
+    max_y = std::max(max_y, s.y);
 
-      lines.push_back(std::make_pair(s, e));
+    Point e;
+    std::stringstream ee(std::move(third_column));
+    std::getline(ee, number, ',');
+    e.x = stoi(number);
+    std::getline(ee, number);
+    e.y = stoi(number);
+    min_x = std::min(min_x, e.x);
+    min_y = std::min(min_y, e.y);
+    max_x = std::max(max_x, e.x);
+    max_y = std::max(max_y, e.y);
+
+    lines.push_back(std::make_pair(s, e));
   }
+  fmt::print("{}",lines.size());
 
   const auto it = std::partition(lines.begin(), lines.end(),
                                  [](std::pair<Point, Point> line) {
@@ -59,9 +67,21 @@ int main(int argc, char *argv[])
                                            (line.first.y == line.second.y));
                                  });
   lines.erase(it, lines.end());
+
+  std::transform(lines.begin(), lines.end(),
+                 lines.begin(), [&](std::pair<Point, Point> p){
+                   auto xs = p.first.x - min_x;
+                   auto ys = p.first.y - min_y;
+                   auto xe = p.second.x - min_x;
+                   auto ye = p.second.y - min_y;
+                   return std::make_pair(Point({xs, ys}), Point({xe, ye}));
+                 });
   
-  max_x = max_x + 1;
-  max_y = max_y + 1;
+  fmt::print("min {}, {}, max {}, {}\n", min_x, min_y, max_x, max_y);
+  max_x = max_x - min_x ;
+  max_y = max_y - min_y ;
+
+  fmt::print("min {}, {}, max {}, {}\n", min_x, min_y, max_x, max_y);
   std::vector<int> grid((max_x * max_y), 0);
   for(const auto & [start_point, end_point]:lines)
     {
@@ -98,15 +118,40 @@ int main(int argc, char *argv[])
           ++diff_y;
         }
       }
+      
+      while (diff_x != 0 && diff_y != 0) {
+        if (!std::signbit(diff_y)) {
+          current_x++;
+          current_y++;
+          ++grid[idx()];
+          --diff_x;
+          --diff_y;
+        } else {
+          current_x--;
+          current_y--;
+          ++grid[idx()];
+          ++diff_y;
+          ++diff_y;
+        }
+      }
   }
+
+  int overlapped {0};
 
   for(int y = 0; y < max_y; ++y)
     {
       for(int x = 0; x < max_x; ++x)
         {
           const auto index = x + y * max_y;
-            fmt::print("{} ", grid[index]);
+          if(grid[index] != 0 && grid[index] != 1)
+            {
+              ++overlapped;
+            }
         }
-      fmt::print("\n");
     }
+    const auto its =
+      std::min_element(grid.begin(), grid.end());
+
+    fmt::print("min = {}\n", std::distance(grid.begin(), its));
+    fmt::print("overlapped: {}\n", overlapped);
 }
